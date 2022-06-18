637e5e9a5126985b24839ddd2bee63f23304d96f
四叶草 <2801819967@qq.com>
2022-06-18 23:37:56 +0800

    全平台实现了bukkit 创建了Sponge模块

---
+++ sponge/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.model.PluginDependency
+
+plugins {
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "2.0.2"
+}
+
+group = "cn.jja8"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sponge {
+    apiVersion("9.0.0")
+    license("All Rights Reserved")
+    loader {
+        name(PluginLoaders.JAVA_PLAIN)
+        version("1.0")
+    }
+    plugin("knapsacktogo4") {
+        displayName("KnapsackToGo4")
+        entrypoint("cn.jja8.knapsackToGo4.sponge.KnapsackToGo4")
+                description("My plugin description")
+                links {
+                        // homepage("https://spongepowered.org")
+                        // source("https://spongepowered.org/source")
+            // issues("https://spongepowered.org/issues")
+        }
+                dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+            }
+}
+
+val javaTarget = 17 // Sponge targets a minimum of Java 17
+java {
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+    if (JavaVersion.current() < JavaVersion.toVersion(javaTarget)) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(javaTarget))
+    }
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
