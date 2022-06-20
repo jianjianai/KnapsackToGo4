714f135e22223485fd526d4c5ed0b7303426145e
四叶草 <2801819967@qq.com>
2022-06-21 00:18:46 +0800

    修复报错时的空指针问题

--- all/src/main/java/cn/jja8/knapsackToGo4/all/work/error/KnapsackToGo4Error.java
+++ all/src/main/java/cn/jja8/knapsackToGo4/all/work/error/KnapsackToGo4Error.java
@@ -18,11 +18,35 @@ public class KnapsackToGo4Error extends Error{
         this.cause = cause;
     }
 
+    /**
+     * 不推荐
+     * */
+    public KnapsackToGo4Error(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    /**
+     * 不推荐
+     * */
+    public KnapsackToGo4Error(Throwable cause,String message) {
+        super(message);
+        this.message = message;
+        this.cause = cause;
+    }
+
     @Override
     public void printStackTrace() {
-        logger.severe("插件运行时发生异常，原因："+ message);
-        logger.severe("下方是详细异常信息，如需反馈请将其完整反馈给开发者。");
+        if (logger!=null){
+            logger.severe("插件运行时发生异常，原因："+ message);
+            logger.severe("下方是详细异常信息，如需反馈请将其完整反馈给开发者。");
+        }else {
+            System.out.println("插件运行时发生异常，原因："+ message);
+            System.out.println("下方是详细异常信息，如需反馈请将其完整反馈给开发者。");
+        }
         super.printStackTrace();
-        cause.printStackTrace();
+        if (cause!=null){
+            cause.printStackTrace();
+        }
     }
 }
