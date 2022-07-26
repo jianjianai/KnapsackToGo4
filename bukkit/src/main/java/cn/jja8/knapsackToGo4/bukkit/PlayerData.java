17fb13f29fa55c9c2eea44d5be95a2a4f15b56a1
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-07-26 23:20:53 +0800

    real final

--- bukkit/src/main/java/cn/jja8/knapsackToGo4/bukkit/PlayerData.java
+++ bukkit/src/main/java/cn/jja8/knapsackToGo4/bukkit/PlayerData.java
@@ -28,12 +28,12 @@ public class PlayerData {
             if (playerDataCase==null){
                 String playerDataCase = null;
                 try {
-                    playerDataCase = YamlConfig.loadFromFile(new File(KnapsackToGo4.INSTANCE.getDataFolder(),"PlayerDataStorage.yml"), new PlayerDataSettings()).playerDataCaseType;
+                    playerDataCase = YamlConfig.loadFromFile(new File(KnapsackToGo4.INSTANCE.getDataFolder(),"PlayerDataSettings.yml"), new PlayerDataSettings()).playerDataCaseType;
                     PlayerDataCaseType playerDataCaseType = PlayerDataCaseType.valueOf(playerDataCase);
                     PlayerData.playerDataCase = playerDataCaseType.getDataCase();
                     KnapsackToGo4.INSTANCE.getLogger().info("已加载内置数据容器。["+playerDataCase+"]");
                 } catch (IllegalArgumentException e){
-                    throw new NoOptionsError(e,"加载配置文件PlayerDataStorage.yml中"+playerDataCase+"值不存在！");
+                    throw new NoOptionsError(e,"加载配置文件PlayerDataSettings.yml中"+playerDataCase+"值不存在！");
                 }
             } else {
                 KnapsackToGo4.INSTANCE.getLogger().info("已加载数据容器扩展。["+playerDataCase.getClass().getName()+"]");
@@ -41,12 +41,12 @@ public class PlayerData {
             if (playerDataSerialize==null){
                 String playerDataSerialize = null;
                 try {
-                    playerDataSerialize = YamlConfig.loadFromFile(new File(KnapsackToGo4.INSTANCE.getDataFolder(),"PlayerDataStorage.yml"), new PlayerDataSettings()).playerDataSerializeType;
+                    playerDataSerialize = YamlConfig.loadFromFile(new File(KnapsackToGo4.INSTANCE.getDataFolder(),"PlayerDataSettings.yml"), new PlayerDataSettings()).playerDataSerializeType;
                     PlayerDataSerializeType playerDataSerializeType = PlayerDataSerializeType.valueOf(playerDataSerialize);
                     PlayerData.playerDataSerialize = playerDataSerializeType.getYamlDataSerialize();
                     KnapsackToGo4.INSTANCE.getLogger().info("已加载内置数序列化方法。["+playerDataSerialize+"]");
                 } catch (IllegalArgumentException e){
-                    throw new NoOptionsError(e,"加载配置文件PlayerDataStorage.yml中"+playerDataSerialize+"值不存在！");
+                    throw new NoOptionsError(e,"加载配置文件PlayerDataSettings.yml中"+playerDataSerialize+"值不存在！");
                 }
             }else {
                 KnapsackToGo4.INSTANCE.getLogger().info("已加载数序列化方法扩展。["+playerDataSerialize.getClass().getName()+"]");
